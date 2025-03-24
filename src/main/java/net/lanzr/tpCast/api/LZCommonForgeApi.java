5e8813bb6ca77ae01b177f2665b17dce587b16ff
Lanzr <mageman@126.com>
2025-03-24 14:45:06 +0800

    增加mark功能等

--- src/main/java/net/lanzr/tpCast/api/LZCommonForgeApi.java
+++ src/main/java/net/lanzr/tpCast/api/LZCommonForgeApi.java
@@ -1,15 +1,15 @@
 package net.lanzr.tpCast.api;
 
-import it.unimi.dsi.fastutil.floats.Float2ObjectArrayMap;
 import net.minecraft.ChatFormatting;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.common.MinecraftForge;
-
-import javax.swing.*;
+import net.minecraft.world.phys.Vec3;
+import org.apache.commons.lang3.tuple.Pair;
 
 public class LZCommonForgeApi {
     public static enum MsgTypes {
@@ -32,5 +32,15 @@ public class LZCommonForgeApi {
     }
     public static ServerLevel playerGetLevel(ServerPlayer player) {
         return player.serverLevel();
+    }
+
+    public static ResourceLocation getDimensionResourceLocation(String name) {
+        return new ResourceLocation(name);
+    }
+
+    public static void PairParseTeleport(ServerPlayer player, Pair<Vec3,String> cp) {
+        ResourceLocation rl = getDimensionResourceLocation(cp.getRight());
+        ResourceKey<Level> dim = ResourceKey.create(Registries.DIMENSION,rl);
+        player.teleportTo(player.getServer().getLevel(dim), cp.getLeft().x,cp.getLeft().y+1,cp.getLeft().z,player.getYRot(),player.getXRot());
     }
 }
--- src/main/java/net/lanzr/tpCast/api/tpCastTag.java
+++ src/main/java/net/lanzr/tpCast/api/tpCastTag.java
@@ -8,22 +8,22 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.phys.Vec3;
 import org.apache.commons.lang3.tuple.Pair;
 
-import java.awt.desktop.PrintFilesEvent;
-import java.text.CollationElementIterator;
+import java.util.Set;
 
 public class tpCastTag {
-    private CompoundTag mTag;
-    private ServerPlayer mPlayer;
+    private final CompoundTag mTag;
+    private final ServerPlayer mPlayer;
+    private final CompoundTag mMarkList;
     final public int CoolDownPiece = Config.levelCoolDownPerLevel * 10; // 10 = 1s
     final public int MaxLevel = Config.maxLevel;
     final public long MaxCoolDown = MaxLevel *CoolDownPiece;
     final private double punishLevel = Config.levelPunishFuseBlow;
-    public String
-            HomePosAlias ="homePos",
-            HomeDimAlias ="homeDim",
-            BackPosAlias ="backPos",
-            BackDimAlias ="backDim",
-            CoolDownStampAlias="stamp";
+    public final String
+            HomePosAlias ="homePos";
+    public final String HomeDimAlias ="homeDim";
+    public final String BackPosAlias ="backPos";
+    public final String BackDimAlias ="backDim";
+    public final String CoolDownStampAlias="stamp";
     public tpCastTag(ServerPlayer player) {
         CompoundTag pTag = player.getPersistentData();
         mTag  = pTag.getCompound(tpCast.MODID);
@@ -36,8 +36,13 @@ public class tpCastTag {
             mTag.putLong(CoolDownStampAlias,LZCommonForgeApi.playerGetLevel(player).getGameTime());
         }
 
+        if(mTag.contains("mark")) {
+            mMarkList = mTag.getCompound("mark");
+        }else {
+            mTag.put("mark",mMarkList = new CompoundTag());
+        }
     }
-    public boolean castOverload(float level) {
+    public void castOverload(float level) {
         boolean ret = true;
         long st = mTag.getLong(CoolDownStampAlias);
         long gt = LZCommonForgeApi.playerGetLevel(mPlayer).getGameTime();
@@ -47,7 +52,6 @@ public class tpCastTag {
             ret = false;
         }
         setCoolDownStamp(st+overLoad);
-        return ret;
     }
 
     public boolean hasKey(String str){
@@ -107,6 +111,42 @@ public class tpCastTag {
         Vec3 vec = new Vec3(pos[0],pos[1],pos[2]);
         String dim = mTag.getString(BackDimAlias);
         return Pair.of(vec, dim);
+    }
+
+    public void setMark(String name) {
+        BlockPos playerPos = mPlayer.getOnPos();
+        CompoundTag mark = null;
+        if (!mMarkList.contains(name)) {
+            mMarkList.put(name,mark = new CompoundTag());
+        } else {
+            mark = mMarkList.getCompound(name);
+        }
+        mark.putIntArray("pos",new int[]{playerPos.getX(), playerPos.getY(), playerPos.getZ()});
+        mark.putString("dim", LZCommonForgeApi.playerGetLevel(mPlayer).dimension().location().toString());
+    }
+    public Set<String> getMarks() {
+        Set<String> keys = mMarkList.getAllKeys();
+        for (String key : keys) {
+            System.out.println(key);
+        }
+        return keys;
+    }
+    public Pair<Vec3, String> getMark(String name) {
+        // 不存在返回null
+        if(!mMarkList.contains(name))
+            return null;
+        CompoundTag mark = mMarkList.getCompound(name);
+        int[] pos =  mark.getIntArray("pos");
+        Vec3 vec = new Vec3(pos[0],pos[1],pos[2]);
+        String dim = mark.getString("dim");
+        return Pair.of(vec, dim);
+    }
+
+    public void rmMark(String name) {
+        if(mMarkList.contains(name)) {
+            mMarkList.remove(name);
+        }
+
     }
 
 }
